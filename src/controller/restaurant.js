3a7b5e2e8eb79a0c65c6583940b47e30a5f521de
vincent_g <vincent.grossier@infostrates.fr>
2017-01-27 16:00:58 +0100

    Update method added.

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -22,15 +22,41 @@ export default({ config, db }) => {
         });
     });
 
-    // '/v1/restaurant' - Read
+    // '/v1/restaurant' - Read.
     api.get('/', (req, res) => {
         Restaurant.find({}, (err, restaurants) => {
             if (err) {
                 res.send(err);
             }
             res.json(restaurants);
-        })
-    })
+        });
+    });
+
+    // '/v1/restaurant/:id' - Read.
+    api.get('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(restaurant);
+        });
+    });
+
+    // '/v1/restaurant/:id' - Update.
+    api.put('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            restaurant.name = req.body.name;
+            restaurant.save(err => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({ message: 'Restaurant info updated.'});
+            });
+        });
+    });
 
     return api;
 }
